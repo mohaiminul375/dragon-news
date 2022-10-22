3b025cd2219e0b8c38ef5828a3836b46d117b22f
mohaiminul375 <mohaiminul375@gmail.com>
2022-10-23 01:46:45 +0600

    create router and components

---
+++ src/Pages/Shared/Header/LeftSideNav/LeftsideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftsideNav = () => {
+    return (
+        <div>
+            <h2>left side nav</h2>
+        </div>
+    );
+};
+
+export default LeftsideNav;
---
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h1>rigt side nave</h1>
+        </div>
+    );
+};
+
+export default RightSideNav;
